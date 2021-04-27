bce085f1e20c07439df83df3c41fc4d08718b227
baguskhrisnanda <baguskhrisnanda@gmail.com>
2021-04-27 11:33:54 +0800

    Fragment-App

---
+++ app/src/main/java/com/example/testfragment/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.testfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnFragment1, btnFragment2, btnFragment3, btnFragment4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment1 = findViewById(R.id.btn_fragment1);
+        btnFragment2 = findViewById(R.id.btn_fragment2);
+        btnFragment3 = findViewById(R.id.btn_fragment3);
+        btnFragment4 = findViewById(R.id.btn_fragment4);
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new HomeFragment());
+                ft.commit();
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new SettingFragment());
+                ft.commit();
+            }
+        });
+
+        btnFragment3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new AboutFragment());
+                ft.commit();
+            }
+        });
+
+        btnFragment4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new ExitFragment());
+                ft.commit();
+            }
+        });
+    }
+}
